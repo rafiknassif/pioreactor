0da7e195d63875837f9c2ec57819f4b13ccf73cf
rafiknassif <rafikemil@gmail.com>
2025-01-05 21:59:08 -0500

    Update temperature_automation.py

--- pioreactor/background_jobs/temperature_automation.py
+++ pioreactor/background_jobs/temperature_automation.py
@@ -233,7 +233,7 @@ class TemperatureAutomationJob(AutomationJob):
         if new_duty_cycle < 10.0:
             new_duty_cycle = 0.0
         # clamp to [required range], round to two decimals
-        self.heater_duty_cycle = clamp(0.0, round(float(new_duty_cycle), 2), 1.0)
+        self.heater_duty_cycle = clamp(0.0, round(float(new_duty_cycle), 3), .5)
         self.pwm.change_duty_cycle(self.heater_duty_cycle)
 
         if self.heater_duty_cycle == 0.0:
@@ -288,10 +288,10 @@ class TemperatureAutomationJob(AutomationJob):
         self.temperature_timer.unpause()
 
     def setup_pwm(self) -> PWM:
-        hertz = 16  # technically this doesn't need to be high: it could even be 1hz. However, we want to smooth it's
+        # technically this doesn't need to be high: it could even be 1hz. However, we want to smooth it's
         # impact (mainly: current sink), over the second. Ex: imagine freq=1hz, dc=40%, and the pump needs to run for
         # 0.3s. The influence of when the heat is on the pump can be significant in a power-constrained system.
-        hertz = .2
+        hertz = .1
         pin = hardware.PWM_TO_PIN[hardware.HEATER_PWM_TO_PIN]
         pwm = PWM(pin, hertz, unit=self.unit, experiment=self.experiment, pubsub_client=self.pub_client)
         pwm.start(0)
