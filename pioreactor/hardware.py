097eae8a378a0782de341a94c5d84aa113c53147
rafiknassif <rafikemil@gmail.com>
2025-03-06 16:06:58 -0500

    Update hardware.py

--- pioreactor/hardware.py
+++ pioreactor/hardware.py
@@ -18,12 +18,12 @@ from pioreactor.whoami import is_testing_env
 
 # PWMs
 # Heater PWM
-HEATER_PWM_TO_PIN: PwmChannel = "5"
+HEATER_PWM_TO_PIN: PwmChannel = "2"
 
 # map between PWM channels and GPIO pins
 PWM_TO_PIN: dict[PwmChannel, GpioPin] = {
     "1": 6 if hardware_version_info == (0, 1) else 17,
-    "5": 13,  # hardware PWM1 available
+    "2": 13,  # hardware PWM1 available
     "3": 16,
     "4": 12,  # hardware PWM0 available
     HEATER_PWM_TO_PIN: 13,  # dedicated to heater changed from 18
