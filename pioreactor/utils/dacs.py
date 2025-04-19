5ad33901bec9378ba9e3d9add220253d942623a3
Thomas Kulin <thomas.kulin@yahoo.ca>
2025-04-18 22:37:52 -0400

    use the correct i2c write command for adafruit lib

--- pioreactor/utils/dacs.py
+++ pioreactor/utils/dacs.py
@@ -47,8 +47,8 @@ class MCP47CxBxx:
         return self.i2c.probe(self.i2cAddress)  # responds with true if device responds
     
     def writei2c(self, command, data):
-        self.i2c.writeto(self.i2cAddress, command)
-        self.i2c.writeto(self.i2cAddress, data)
+        self.i2c.write(command)
+        self.i2c.write(data)
 
     def set_intensity_to(self, channel, intensity):
         # TODO: account for the nonlinear current drive vs dac value here
