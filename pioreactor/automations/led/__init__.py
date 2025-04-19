61e07d1ea5a0ac5ce2d4280bed71947731892923
rafiknassif <rafikemil@gmail.com>
2025-04-19 15:53:10 -0400

    reordering load sequence

--- pioreactor/automations/led/__init__.py
+++ pioreactor/automations/led/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
+from .lightrod_light_control import LightrodLightControl
 from .light_dark_cycle import LightDarkCycle
 from .silent import Silent as LEDSilent
-from .lightrod_light_control import LightrodLightControl
