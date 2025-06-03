20a42b9756766e65a42566487f685527684135e2
Thomas Kulin <thomas.kulin@yahoo.ca>
2025-06-02 22:37:37 -0400

    that did not help idk

--- pioreactor/actions/led_driver.py
+++ pioreactor/actions/led_driver.py
@@ -1,222 +1,3 @@
-# # -*- coding: utf-8 -*-
-# from __future__ import annotations
-
-# from contextlib import contextmanager
-# from contextlib import nullcontext
-# from os import getpid
-# from typing import Any
-# from typing import Iterator
-
-# import click
-# from msgspec.json import encode
-
-# from pioreactor import structs
-# from pioreactor.hardware import LR_DAC_ADDR
-# from pioreactor.exc import HardwareNotFoundError
-# from pioreactor.logging import create_logger
-# from pioreactor.pubsub import Client
-# from pioreactor.pubsub import create_client
-# from pioreactor.pubsub import QOS
-# from pioreactor.types import LedDriverChannel, LedIntensityValue, LedDriverCurrent
-# from pioreactor.utils import local_intermittent_storage
-# from pioreactor.utils.timing import current_utc_datetime
-# from pioreactor.whoami import get_assigned_experiment_name
-# from pioreactor.whoami import get_unit_name
-# from pioreactor.whoami import is_active
-# from pioreactor.whoami import is_testing_env
-
-# ALL_DRIVER_CHANNELS: list[LedDriverChannel] = ["DRV_A", "DRV_B"]
-# LEDsToIntensityMapping = dict[LedDriverChannel, LedIntensityValue]
-# LEDsToCurrentMapping = dict[LedDriverChannel, LedDriverCurrent]
-# dac = None
-
-# def initialize_dac():
-#     from pioreactor.utils.dacs import MCP47CxBxx
-#     global dac
-#     dac = MCP47CxBxx(LR_DAC_ADDR, 8)  # Hard coded to 8 bit resolution
-
-# def _update_current_state(
-#     state: LEDsToIntensityMapping,
-# ) -> tuple[LEDsToIntensityMapping, LEDsToIntensityMapping]:
-
-#     with local_intermittent_storage("led_driver") as led_driver_cache:
-#         # rehydrate old cache
-#         old_state: LEDsToIntensityMapping = {
-#             channel: led_driver_cache.get(str(channel), 0.0) for channel in ALL_DRIVER_CHANNELS
-#         }
-
-#         # update cache
-#         for channel, intensity in state.items():
-#             led_driver_cache[channel] = intensity
-
-#         new_state: LEDsToIntensityMapping = {
-#             channel: led_driver_cache.get(str(channel), 0.0) for channel in ALL_DRIVER_CHANNELS
-#         }
-
-#         return new_state, old_state
-
-
-# def led_driver_intensity(
-#     desired_state: LEDsToIntensityMapping | LEDsToCurrentMapping,
-#     unit: str | None = None,
-#     experiment: str | None = None,
-#     verbose: bool = True,
-#     source_of_event: str | None = None,
-#     pubsub_client: Client | None = None,
-# ) -> bool:
-#     """
-#     Change the intensity of the LED Driver channels A and B to an value between 0 and 100.
-
-#     Parameters
-#     ------------
-#     desired_state: dict
-#         what you want the desired LED state to be. Leave keys out if you do wish to update that channel.
-#     unit: str
-#     experiment: str
-#     verbose: bool
-#         if True, log the change, and send event to led_event table & mqtt. This is FALSE
-#         in od_reading job, so as to not create spam.
-#     source_of_event: str
-#         A human readable string of who is calling this function
-#     pubsub_client:
-#         provide a MQTT paho client to use for publishing.
-
-
-#     Returns
-#     --------
-#     bool representing if the all LED channels intensity were successfully changed
-
-
-#     Notes
-#     -------
-#     State is updated in MQTT and the temporary cache `led_driver`:
-
-#         pioreactor/<unit>/<experiment>/leds/intensity    {'A': intensityA, 'B': intensityB, ...}
-
-#     """
-#     unit = unit or get_unit_name()
-#     experiment = experiment or get_assigned_experiment_name(unit)
-
-#     if not is_active(unit):
-#         return False
-
-#     logger = create_logger("led_driver_intensity", experiment=experiment, unit=unit, pub_client=pubsub_client)
-#     updated_successfully = True
-
-
-#     if pubsub_client is None:
-#         mqtt_publishing = create_client(client_id=f"led_driver_intensity-{unit}-{experiment}")
-#         mqtt_publish = mqtt_publishing.publish
-#     else:
-#         mqtt_publishing = nullcontext()
-#         mqtt_publish = pubsub_client.publish
-
-#     with mqtt_publishing:
-
-#         for channel, setpoint in desired_state.items():
-#             try:
-#                 assert (channel in ALL_DRIVER_CHANNELS), f"Saw incorrect channel {channel}, not in {ALL_DRIVER_CHANNELS}"
-#                 if type(setpoint) is LedIntensityValue:
-#                     assert (0.0 <= setpoint <= 100.0), f"Channel {channel} intensity should be between 0 and 100, inclusive"
-#                     dac.set_intensity_to(channel, setpoint)
-#                     logger.info(f"LED Driver Intensity Set")
-#                 elif type(setpoint) is LedDriverCurrent:
-#                     assert (0.0 <= setpoint <= 750.0), f"Channel {channel} current should be between 0 and 750, inclusive"
-#                     dac.set_current_to(channel, setpoint)
-#                     logger.info(f"LED Driver Current Set")
-#             except (ValueError, HardwareNotFoundError) as e:
-#                 logger.debug(e, exc_info=True)
-#                 logger.error(
-#                     "Unable to find i2c for LED driver. Confirm i2c connected. Did the LED driver explode unexpectedly?"
-#                 )
-#                 updated_successfully = False
-#                 return updated_successfully
-#             except AssertionError as e:
-#                 logger.error(e)
-#                 updated_successfully = False
-#                 return updated_successfully
-
-#         new_state, old_state = _update_current_state(desired_state)
-
-#         mqtt_publish(
-#             f"pioreactor/{unit}/{experiment}/leds/driverIntensity",
-#             encode(new_state),
-#             qos=QOS.AT_MOST_ONCE,
-#             retain=True,
-#         )
-
-#         if verbose:
-#             timestamp_of_change = current_utc_datetime()
-
-#             for channel, intensity in desired_state.items():
-#                 if old_state[channel] != new_state[channel]:  # only log on change
-#                     event = structs.LEDChangeEvent(
-#                         channel=channel,
-#                         intensity=intensity,
-#                         source_of_event=source_of_event,
-#                         timestamp=timestamp_of_change,
-#                     )
-
-#                     mqtt_publish(
-#                         f"pioreactor/{unit}/{experiment}/led_driver_change_events",
-#                         encode(event),
-#                         qos=QOS.AT_MOST_ONCE,
-#                         retain=False,
-#                     )
-
-#                     logger.info(
-#                         f"Updated LED Driver {channel} from {old_state[channel]:0.3g}% to {new_state[channel]:0.3g}%."
-#                     )
-#         return updated_successfully
-
-
-# @click.command(name="led_driver_intensity")
-# @click.option(
-#     "--DRV_A",
-#     help="value between 0 and 100",
-#     type=click.FloatRange(0, 100),
-# )
-# @click.option(
-#     "--DRV_B",
-#     help="value between 0 and 100",
-#     type=click.FloatRange(0, 100),
-# )
-# @click.option(
-#     "--source-of-event",
-#     default="CLI",
-#     type=str,
-#     help="who is calling this function (for logging purposes)",
-# )
-# @click.option("--no-log", is_flag=True, help="skip logging")
-# def click_led_intensity(
-#     a: LedIntensityValue | None = None,
-#     b: LedIntensityValue | None = None,
-#     source_of_event: str | None = None,
-#     no_log: bool = False,
-# ) -> bool:
-#     """
-#     Modify the intensity of LED Driver channel(s)
-#     """
-#     unit = get_unit_name()
-#     experiment = get_assigned_experiment_name(unit)
-
-#     state: LEDsToIntensityMapping = {}
-#     if a is not None:
-#         state["DRV_A"] = a
-#     if b is not None:
-#         state["DRV_B"] = b
-
-#     status = led_driver_intensity(
-#         state,
-#         source_of_event=source_of_event,
-#         unit=unit,
-#         experiment=experiment,
-#         verbose=not no_log,
-#     )
-#     return status
-
-
-
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
@@ -236,8 +17,7 @@ from pioreactor.logging import create_logger
 from pioreactor.pubsub import Client
 from pioreactor.pubsub import create_client
 from pioreactor.pubsub import QOS
-from pioreactor.types import LedDriverChannel
-from pioreactor.types import LedIntensityValue
+from pioreactor.types import LedDriverChannel, LedIntensityValue, LedDriverCurrent
 from pioreactor.utils import local_intermittent_storage
 from pioreactor.utils.timing import current_utc_datetime
 from pioreactor.whoami import get_assigned_experiment_name
@@ -247,6 +27,7 @@ from pioreactor.whoami import is_testing_env
 
 ALL_DRIVER_CHANNELS: list[LedDriverChannel] = ["DRV_A", "DRV_B"]
 LEDsToIntensityMapping = dict[LedDriverChannel, LedIntensityValue]
+LEDsToCurrentMapping = dict[LedDriverChannel, LedDriverCurrent]
 dac = None
 
 def initialize_dac():
@@ -276,7 +57,7 @@ def _update_current_state(
 
 
 def led_driver_intensity(
-    desired_state: LEDsToIntensityMapping,
+    desired_state: LEDsToIntensityMapping | LEDsToCurrentMapping,
     unit: str | None = None,
     experiment: str | None = None,
     verbose: bool = True,
@@ -332,15 +113,17 @@ def led_driver_intensity(
 
     with mqtt_publishing:
 
-        for channel, intensity in desired_state.items():
+        for channel, setpoint in desired_state.items():
             try:
-                assert (
-                    channel in ALL_DRIVER_CHANNELS
-                ), f"Saw incorrect channel {channel}, not in {ALL_DRIVER_CHANNELS}"
-                assert (
-                    0.0 <= intensity <= 100.0
-                ), f"Channel {channel} intensity should be between 0 and 100, inclusive"
-                dac.set_intensity_to(channel, intensity)
+                assert (channel in ALL_DRIVER_CHANNELS), f"Saw incorrect channel {channel}, not in {ALL_DRIVER_CHANNELS}"
+                if type(setpoint) is LedIntensityValue:
+                    assert (0.0 <= setpoint <= 100.0), f"Channel {channel} intensity should be between 0 and 100, inclusive"
+                    dac.set_intensity_to(channel, setpoint)
+                    logger.info(f"LED Driver Intensity Set")
+                elif type(setpoint) is LedDriverCurrent:
+                    assert (0.0 <= setpoint <= 750.0), f"Channel {channel} current should be between 0 and 750, inclusive"
+                    dac.set_current_to(channel, setpoint)
+                    logger.info(f"LED Driver Current Set")
             except (ValueError, HardwareNotFoundError) as e:
                 logger.debug(e, exc_info=True)
                 logger.error(
@@ -366,23 +149,24 @@ def led_driver_intensity(
             timestamp_of_change = current_utc_datetime()
 
             for channel, intensity in desired_state.items():
-                event = structs.LEDChangeEvent(
-                    channel=channel,
-                    intensity=intensity,
-                    source_of_event=source_of_event,
-                    timestamp=timestamp_of_change,
-                )
+                if old_state[channel] != new_state[channel]:  # only log on change
+                    event = structs.LEDChangeEvent(
+                        channel=channel,
+                        intensity=intensity,
+                        source_of_event=source_of_event,
+                        timestamp=timestamp_of_change,
+                    )
 
-                mqtt_publish(
-                    f"pioreactor/{unit}/{experiment}/led_driver_change_events",
-                    encode(event),
-                    qos=QOS.AT_MOST_ONCE,
-                    retain=False,
-                )
+                    mqtt_publish(
+                        f"pioreactor/{unit}/{experiment}/led_driver_change_events",
+                        encode(event),
+                        qos=QOS.AT_MOST_ONCE,
+                        retain=False,
+                    )
 
-                logger.info(
-                    f"Updated LED Driver {channel} from {old_state[channel]:0.3g}% to {new_state[channel]:0.3g}%."
-                )
+                    logger.info(
+                        f"Updated LED Driver {channel} from {old_state[channel]:0.3g}% to {new_state[channel]:0.3g}%."
+                    )
         return updated_successfully
 
 
--- pioreactor/automations/led/lightrod_light_control.py
+++ pioreactor/automations/led/lightrod_light_control.py
@@ -1,196 +1,3 @@
-# from pioreactor.automations.led.base import LEDAutomationJob
-# from pioreactor.background_jobs.base import BackgroundJob
-# from pioreactor.types import LedDriverChannel, LedChannel
-# from pioreactor.structs import LEDDriverIntensity
-# from pioreactor.automations import events
-# from pioreactor.utils import is_pio_job_running
-# from typing import Optional
-# from pioreactor.actions.led_driver import initialize_dac
-# from pioreactor.types import LedIntensityValue
-# from pioreactor.whoami import get_unit_name, get_assigned_experiment_name
-# from pioreactor.utils.timing import RepeatedTimer, current_utc_datetime
-# from pioreactor.pubsub import QOS
-
-
-# import click
-
-
-# class LightrodLightControl(LEDAutomationJob):
-#     """
-#     Lightrod light control automation for managing LED based on ReadLightRodTemps status.
-#     """
-#     instances = []
-#     automation_name: str = "lightrod_light_control"
-#     published_settings = {
-#         "relay_enabled": {"datatype": "float", "settable": True, "unit": "%"},
-#         "DRV_A_intensity": {"datatype": "float", "settable": True, "unit": "%"},
-#         "DRV_B_intensity": {"datatype": "float", "settable": True, "unit": "%"},
-#     }
-
-#     def __init__(
-#         self,
-#         DRV_A_intensity: float | str,
-#         DRV_B_intensity: float | str,
-#         relay_enabled: float | str,
-#         **kwargs,
-#     ):  
-#         self.instances.append(self)
-#         super().__init__(**kwargs)
-#         self.DRV_A_intensity = float(DRV_A_intensity)
-#         self.DRV_B_intensity = float(DRV_B_intensity)
-#         self.relay_enabled = float(relay_enabled)
-#         self.channels: list[LedDriverChannel] = ["DRV_A", "DRV_B"]
-#         self.relayChannel : LedChannel = "B"
-#         self.light_active: bool = False
-#         self.prev_A_intensity = 0
-#         self.prev_B_intensity = 0
-
-#         initialize_dac()
-
-#         # Subscribe to control topic
-#         self.subscribe_and_callback(
-#             self.handle_control_message,
-#             f"pioreactor/{self.unit}/{self.experiment}/lightrod_light_control/control",
-#             qos=QOS.AT_LEAST_ONCE,
-#         )
-
-#         self.unit = get_unit_name()
-#         self.experiment = get_assigned_experiment_name(self.unit)
-    
-#     def on_disconnected(self) -> None:
-#         self.shutdown_drivers()
-#         self.disable_relay()
-#         super().on_disconnected()
-
-#     def execute(self) -> Optional[events.AutomationEvent]:
-#         """
-#         Periodically check ReadLightRodTemps status and adjust LED state accordingly.
-#         If ReadLightRodTemps is not running, log an error and disconnect.
-#         """
-#         self.logger.debug("Executing LightrodLightControl check.")
-
-#         # Check if ReadLightRodTemps is running
-#         is_running = is_pio_job_running("read_lightrod_temps")
-#         self.logger.debug(f"read_lightrod_temps running status: {is_running}")
-
-#         if not is_running:
-#             self.logger.error("ReadLightRodTemps is not running. Disconnecting LED automation.")
-#             self.light_active = False
-#             self.disable_relay()
-#             if self.state != self.DISCONNECTED:
-#                 self.set_state(self.DISCONNECTED)
-#             return events.ChangedLedIntensity("Turned off relay. LEDs disabled due to ReadLightRodTemps not running.")
-        
-#         if not self.light_active and self.relay_enabled == 100:
-#             self.light_active = True
-#             self.enable_relay()
-#             self.logger.info(f"Turned on relay.")
-
-#         if not self.relay_enabled == 100:
-#             self.light_active = False
-#             self.disable_relay()
-#             self.logger.info(f"Turned off relay.")
-
-#         if self.light_active:
-#             self.set_driver_intensity()
-
-#         return None
-    
-#     def handle_control_message(self, message):
-#         command = message.payload.decode()
-#         if command == "shutdown_drivers":
-#             self.shutdown_drivers()
-#         elif "DRV_A_intensity" in command:
-#             _, param = command.split(" ", 1)
-#             self.DRV_A_intensity = float(param)
-#             self.set_driver_intensity()
-#         elif "DRV_B_intensity" in command:
-#             _, param = command.split(" ", 1)
-#             self.DRV_B_intensity = float(param)
-#             self.set_driver_intensity()
-#         else:
-#             self.logger.warning(f"Unknown command: {command}")
-    
-#     def disable_relay(self):
-#         self.set_led_intensity(self.relayChannel, 0)  # turn off the relay
-#         self.logger.debug(f"Disable LED relay")
-
-#     def enable_relay(self):
-#         self.set_led_intensity(self.relayChannel, 100)  # turn on the relay 
-#         self.logger.debug(f"Enable LED relay")
-
-#     def shutdown_drivers(self):
-#         self.DRV_A_intensity = 0
-#         self.DRV_B_intensity = 0
-#         self.set_driver_intensity()
-#         self.logger.debug("shutdown drivers called - setting intensities to 0")
-
-#     def set_driver_intensity(self):
-#         """
-#         Update light intensity for the bioreactor.
-#         """
-#         if self.DRV_A_intensity==0 and self.DRV_B_intensity==0:
-#             self.disable_relay()
-
-#         if self.light_active:
-#             self.set_led_driver_intensity(self.channels[0], self.DRV_A_intensity)
-#             self.logger.info(f"LED Driver Intensity Set")
-#             if self.DRV_A_intensity != self.prev_A_intensity:
-#                 self.prev_A_intensity = self.DRV_A_intensity
-#                 self.publish_intensity(self.channels[0], self.DRV_A_intensity)
-#                 self.logger.debug(f"Set LED channel {self.channels[0]} to an intensity of {self.DRV_A_intensity}")
-
-#             self.set_led_driver_intensity(self.channels[1], self.DRV_B_intensity)
-#             if self.DRV_B_intensity != self.prev_B_intensity:
-#                 self.prev_B_intensity = self.DRV_B_intensity
-#                 self.publish_intensity(self.channels[1], self.DRV_B_intensity)
-#                 self.logger.debug(f"Set LED channel {self.channels[1]} to an intensity of {self.DRV_B_intensity}")
-
-#     def publish_intensity(self, channel, intensity):
-#         driverIntensity = LEDDriverIntensity(
-#             timestamp=current_utc_datetime(),
-#             channel=channel,
-#             driver_intensity=intensity
-#         )
-#         BackgroundJob.publish(
-#             self,
-#             topic=f"pioreactor/{self.unit}/{self.experiment}/lightrod_light_control/driver_intensity",
-#             payload=driverIntensity  # Publish as an object
-#         )
-
-#     @classmethod
-#     def getInstance(cls):
-#         return cls.instances[0] if cls.instances else None
-
-# import click
-
-# @click.command(name="lightrod_light_control")
-# @click.option(
-#     "--DRV_A_intensity",
-#     default=0,
-#     show_default=True,
-#     type=click.FloatRange(0, 100, clamp=True),
-# )
-# @click.option(
-#     "--DRV_B_intensity",
-#     default=0,
-#     show_default=True,
-#     type=click.FloatRange(0, 100, clamp=True),
-# )
-# def click_lightrod_light_control(DRV_A_SETPOINT, DRV_B_SETPOINT):
-#     unit = get_unit_name()
-#     experiment = get_assigned_experiment_name(unit)
-
-#     from pioreactor.logging import create_logger
-#     logger = create_logger("click_lightrod_light_control")
-#     logger.debug(f"updated led driver setpoints: {DRV_A_SETPOINT}, {DRV_B_SETPOINT}")
-
-#     inst = LightrodLightControl.getInstance()
-#     logger.debug(f"updating lightrodLightControl instance: {inst.__repr__()}")
-#     inst.DRV_A_intensity = float(DRV_A_SETPOINT)
-#     inst.DRV_B_intensity = float(DRV_B_SETPOINT)
-
-
 from pioreactor.automations.led.base import LEDAutomationJob
 from pioreactor.background_jobs.base import BackgroundJob
 from pioreactor.types import LedDriverChannel, LedChannel
@@ -202,6 +9,8 @@ from pioreactor.actions.led_driver import initialize_dac
 from pioreactor.types import LedIntensityValue
 from pioreactor.whoami import get_unit_name, get_assigned_experiment_name
 from pioreactor.utils.timing import RepeatedTimer, current_utc_datetime
+from pioreactor.pubsub import QOS
+
 
 import click
 
@@ -233,10 +42,25 @@ class LightrodLightControl(LEDAutomationJob):
         self.channels: list[LedDriverChannel] = ["DRV_A", "DRV_B"]
         self.relayChannel : LedChannel = "B"
         self.light_active: bool = False
+        self.prev_A_intensity = 0
+        self.prev_B_intensity = 0
+
         initialize_dac()
+
+        # Subscribe to control topic
+        self.subscribe_and_callback(
+            self.handle_control_message,
+            f"pioreactor/{self.unit}/{self.experiment}/lightrod_light_control/control",
+            qos=QOS.AT_LEAST_ONCE,
+        )
 
         self.unit = get_unit_name()
         self.experiment = get_assigned_experiment_name(self.unit)
+    
+    def on_disconnected(self) -> None:
+        self.shutdown_drivers()
+        self.disable_relay()
+        super().on_disconnected()
 
     def execute(self) -> Optional[events.AutomationEvent]:
         """
@@ -272,6 +96,21 @@ class LightrodLightControl(LEDAutomationJob):
 
         return None
     
+    def handle_control_message(self, message):
+        command = message.payload.decode()
+        if command == "shutdown_drivers":
+            self.shutdown_drivers()
+        elif "DRV_A_intensity" in command:
+            _, param = command.split(" ", 1)
+            self.DRV_A_intensity = float(param)
+            self.set_driver_intensity()
+        elif "DRV_B_intensity" in command:
+            _, param = command.split(" ", 1)
+            self.DRV_B_intensity = float(param)
+            self.set_driver_intensity()
+        else:
+            self.logger.warning(f"Unknown command: {command}")
+    
     def disable_relay(self):
         self.set_led_intensity(self.relayChannel, 0)  # turn off the relay
         self.logger.debug(f"Disable LED relay")
@@ -280,21 +119,32 @@ class LightrodLightControl(LEDAutomationJob):
         self.set_led_intensity(self.relayChannel, 100)  # turn on the relay 
         self.logger.debug(f"Enable LED relay")
 
+    def shutdown_drivers(self):
+        self.DRV_A_intensity = 0
+        self.DRV_B_intensity = 0
+        self.set_driver_intensity()
+        self.logger.debug("shutdown drivers called - setting intensities to 0")
+
     def set_driver_intensity(self):
         """
         Update light intensity for the bioreactor.
         """
-        if all(i==0 for i in self.drv_intensity):
+        if self.DRV_A_intensity==0 and self.DRV_B_intensity==0:
             self.disable_relay()
 
         if self.light_active:
-            self.set_led_driver_intensity(self.channels[0], self.drv_A_intensity)
-            self.publish_intensity(self.channels[0], self.drv_A_intensity)
-            self.logger.debug(f"Set LED channel {self.channels[0]} to an intensity of {self.drv_A_intensity}")
+            self.set_led_driver_intensity(self.channels[0], self.DRV_A_intensity)
+            self.logger.info(f"LED Driver Intensity Set")
+            if self.DRV_A_intensity != self.prev_A_intensity:
+                self.prev_A_intensity = self.DRV_A_intensity
+                self.publish_intensity(self.channels[0], self.DRV_A_intensity)
+                self.logger.debug(f"Set LED channel {self.channels[0]} to an intensity of {self.DRV_A_intensity}")
 
-            self.set_led_driver_intensity(self.channels[1], self.drv_B_intensity)
-            self.publish_intensity(self.channels[1], self.drv_B_intensity)
-            self.logger.debug(f"Set LED channel {self.channels[1]} to an intensity of {self.drv_B_intensity}")
+            self.set_led_driver_intensity(self.channels[1], self.DRV_B_intensity)
+            if self.DRV_B_intensity != self.prev_B_intensity:
+                self.prev_B_intensity = self.DRV_B_intensity
+                self.publish_intensity(self.channels[1], self.DRV_B_intensity)
+                self.logger.debug(f"Set LED channel {self.channels[1]} to an intensity of {self.DRV_B_intensity}")
 
     def publish_intensity(self, channel, intensity):
         driverIntensity = LEDDriverIntensity(
@@ -308,9 +158,9 @@ class LightrodLightControl(LEDAutomationJob):
             payload=driverIntensity  # Publish as an object
         )
 
-@classmethod
-def getInstance(cls):
-    return cls.instances[0]
+    @classmethod
+    def getInstance(cls):
+        return cls.instances[0] if cls.instances else None
 
 import click
 
@@ -339,3 +189,4 @@ def click_lightrod_light_control(DRV_A_SETPOINT, DRV_B_SETPOINT):
     logger.debug(f"updating lightrodLightControl instance: {inst.__repr__()}")
     inst.DRV_A_intensity = float(DRV_A_SETPOINT)
     inst.DRV_B_intensity = float(DRV_B_SETPOINT)
+
