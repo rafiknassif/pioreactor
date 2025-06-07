536dab8468aa04a4a425568b9f0f393efb401f64
Thomas Kulin <thomas.kulin@yahoo.ca>
2025-06-07 13:22:25 -0400

    add support for the floats in mqtt publish code

--- pioreactor/background_jobs/base.py
+++ pioreactor/background_jobs/base.py
@@ -921,9 +921,17 @@ class _BackgroundJob(metaclass=PostInitCaller):
             raise RuntimeError(f"{self.job_name} is already running. Skipping.")
 
     def __setattr__(self, name: str, value: t.Any) -> None:
-        super(_BackgroundJob, self).__setattr__(name, value)
+        # super(_BackgroundJob, self).__setattr__(name, value)
+        # if name in self.published_settings:
+        #     self._publish_setting(name)
         if name in self.published_settings:
-            self._publish_setting(name)
+            # Convert LedIntensityValue to float for publishing
+            publish_value = value.value if isinstance(value, LedIntensityValue) else value
+            super().__setattr__(name, value)
+            if self.state != self.DISCONNECTED:
+                self._publish_setting(name, publish_value)
+        else:
+            super().__setattr__(name, value)
 
     def __enter__(self: BJT) -> BJT:
         return self
